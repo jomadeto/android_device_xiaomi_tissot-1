bb3c02f629527536259d78b84c45d22984a69b15
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2020-09-18 04:18:14 +0000

    set hal dir as well as remove static boot ctrl hal

--- device.mk
+++ device.mk
@@ -46,16 +46,20 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.msm8953.recovery
-
-PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl.recovery \
     bootctrl.msm8953 \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
+
+PRODUCT_PACKAGES += \
+    bootctrl.msm8953.recovery \
+    bootctrl.msm8953 \
+    libcutils \
+    libgptutils.tissot.recovery \
+    libz
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
@@ -89,6 +93,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    hardware/qcom-caf/msm8996/display \
+    hardware/qcom-caf/msm8996/audio \
+    hardware/qcom-caf/msm8996/media
 
 # Touch HAL
 PRODUCT_PACKAGES += \
